7e3e5223530156486dd3f800ff0b3b203c562389
dianxia <834102823@qq.com>
2013-07-14 11:16:28 +0800

    after_rake_db_seed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'f43f0268fdb6755a4416e4a8be0d5241685550784b1e1d60b3f82c83910c32c547a3250731aefb9bb65d377065bc723ad5df5e2bba20ff4f7276992ab2ddf999'
