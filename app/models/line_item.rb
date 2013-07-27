b91105fd53e3b446cfed9a9ed9ac97cd17d3ee9d
dianxia <834102823@qq.com>
2013-07-27 11:34:37 +0800

    0727_1134_finish_12.1

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,9 +2,10 @@ class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id
   belongs_to :product
   belongs_to :cart
+  belongs_to :order
   attr_accessible :product
   attr_accessible :quantity
-  
+  attr_accessible :cart
   def total_price
     product.price * quantity
   end
