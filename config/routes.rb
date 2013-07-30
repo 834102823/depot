f0398c6735e2e416a6878b56c1f8320b52d588b7
dianxia <834102823@qq.com>
2013-07-30 16:58:52 +0800

    0730_1658_after_finish_task_j

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Depot::Application.routes.draw do
     delete 'logout'=> :destroy
   end
 
-
+scope '(:locale)' do
   resources :users
 
 
@@ -84,4 +84,5 @@ Depot::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+end
 end
